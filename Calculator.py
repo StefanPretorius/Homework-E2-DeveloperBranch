965810feedf44a6421a085c23ada818b7e1a490b
Stefan Pretorius <stefanpretorius0313@gmail.com>
2023-07-30 17:31:05 +0200

    Added Devide feature to program

--- Calculator.py
+++ Calculator.py
@@ -6,3 +6,9 @@ prod = val1 * val2
 print(prod)
 minus = val1 - val2
 print(minus)
+#Add features while on develloper branch
+if val2 != 0 :
+    devide = val1/val2
+else :
+    print("Can't devide by 0.")
+print(devide)
