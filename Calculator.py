75b878591d348941ba8cb3085c6a5f1d61121309
Stefan Pretorius <stefanpretorius0313@gmail.com>
2023-07-31 09:08:33 +0200

    Added whole to check second value
    Added while to check that the second value is > 0 and removed the If-statement because it is redundant.

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,10 @@
+#Add features while on develloper branch
 val1 = int(input("Please enter the first number: "))
 val2 = int(input("Please enter the second number: "))
+#Add features while on develloper branch
+while val2 <=0:
+    val2 = int(input("Please enter the second number(Must be > 0): "))
+#Display results
 
 sum = val1 + val2
 print(sum)
@@ -8,8 +13,5 @@ print(prod)
 minus = val1 - val2
 print(minus)
 #Add features while on develloper branch
-if val2 != 0 :
-    devide = val1/val2
-else :
-    print("Can't devide by 0.")
+devide = val1/val2
 print(devide)
